6a37d12bd42cc0c34c6e024dc36ba5531ee8567d
Igor Bulanov <bulanov.ig@phystech.edu>
2025-03-30 19:02:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,89 @@
-let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp
 
 tg.expand()
 
 tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#2cab37"
+tg.MainButton.color = "#FF8979"
 
-let btn1 = document.querySelector('#btn1')
-let btn2 = document.querySelector('#btn2')
-let btn3 = document.querySelector('#btn3')
-let btn4 = document.querySelector('#btn4')
-
+let items = {
+    carrot: 0,
+    potato: 0,
+    cucumber: 0,
+    tomato: 0
+}
 let price = 0
 let name = ""
 let phone = ""
-let e-mail = ""
+let email = ""
 
-let items = {
-    MC: 0,
-    PK: 0,
-    KK: 0,
-    PB: 0
-}
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
 
-let usercard = document.querySelector('#usercard')
-let username = document.querySelector('#user-name')
-let userphone = document.querySelector('#user-phone')
-let useremail = document.querySelector('#user-email')
+let usercard = document.querySelector("#usercard")
+let username = document.querySelector("#user-name")
+let useremail = document.querySelector("#user-email")
+let userphone = document.querySelector("#user-phone")
 
 function create_li(text) {
     let li = document.createElement("li")
-    li.innerHTML = Text
+    li.innerHTML = text
     usercard.appendChild(li)
 }
 
-btn1.onclick = () => {
-    items['MC'] += 1
-    price += 100
-    update_orders()
+function update_orders() {
+    usercard.innerHTML = "Ваши заказы:"
+    if (name != "") {
+        create_li("Имя: " + name)
     }
+    if (phone != "") {
+        create_li("Телефон: " + phone)
+    }
+    if (email != "") {
+        create_li("Email: " + email)
+    }
+    for (let item in items) {
+        if (items[item] != 0) {
+            create_li(item + ": " + items[item])
+        }
+    }
+}
+
+username.onchange = () => {
+    name = username.value
+}
+
+useremail.onchange = () => {
+    email = useremail.value
+}
+
+userphone.onchange = () => {
+    phone = userphone.value
+}
+
+btn1.onclick = () => {
+    items['carrot'] += 1
+    price += 5
+    update_orders()
 }
 
 btn2.onclick = () => {
-    items['РК'] += 1
-    price += 10
+    items['potato'] += 1
+    price += 3
     update_orders()
-    }
 }
 
 btn3.onclick = () => {
-    items['КК'] += 1
-    price += 50
+    items['cucumber'] += 1
+    price += 4
     update_orders()
-
 }
 
 btn4.onclick = () => {
-   items['PB'] += 1
-   price += 30
-   update_orders()
+    items['tomato'] += 1
+    price += 6
+    update_orders()
 }
 
 let submit = document.querySelector("#submit")
@@ -67,69 +93,23 @@ submit.onclick = () => {
     tg.MainButton.show()
 }
 
-function update_orders() {
-    usercard.innerHTML = "Ваши заказы: "
-    if (name != "") {
-        create_li("Имя: " + name)
-    }
-    if (phone != "") {
-        create_li("Имя: " + phone)
-    }
-    if (email != "") {
-        create_li("Имя: " + email)
-    }
-    for (let item in items) {
-        if (items[item] != 0) {
-            let li = document.createElement("li")
-            li.innerHTML = item + ": " + items[item]
-            usercard.appendChild(li)
-        }
-    }
-}
-
-username.onchange() = () => {
-    name = username.value
-}
-
-useremail.onchange() = () => {
-    email = useremail.value
-}
-
-userphone.onchange() = () => {
-    phone = userphone.value
-}
-
-
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
     result = ""
     if (name != "") {
-        create_li("Имя: " + name)
-    }
-    if (phone != "") {
-        create_li("Имя: " + phone)
+        result += "Ваше имя: " + name + "\n"
     }
     if (email != "") {
-        create_li("Имя: " + email)
+        result += "Ваше email: " + email + "\n"
     }
-
+    if (phone != "") {
+        result += "Ваше телефон: " + phone + "\n"
+    }
     result += "Ваши заказы: \n"
     for (let item in items) {
         if (items[item] != 0) {
-            result += item + " : " + items[item] + "\n"
+            result += item + ": " + items[item] + "\n"
         }
     }
-    result += "\n\n С вас " + price + "$"
+    result += "\n\nС вас " + price + "$"
     tg.sendData(result)
 })
-
-
-
-
-
-
-
-
-
-
-
-
